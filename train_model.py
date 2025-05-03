79dd7b67faae8707c22bcc899fd7887f9068d226
Yizhen Zang <Y.Zang-3@student.tudelft.nl>
2025-05-03 19:18:02 +0200

    Fix the path to the vectorizer

--- train_model.py
+++ train_model.py
@@ -1,7 +1,7 @@
 import pickle
 from libml.preprocessing import preprocess_train
 from sklearn.naive_bayes import GaussianNB
-
+import shutil
 
 def train_and_save_model(data_path, model_path, test_size=0.2, random_state=0):
     # Preprocess using `lib-ml``
@@ -10,6 +10,7 @@ def train_and_save_model(data_path, model_path, test_size=0.2, random_state=0):
     # Train model
     model = GaussianNB()
     model.fit(X_train, y_train)
+    shutil.copy("../c1_BoW_Sentiment_Model.pkl", "c1_BoW_Sentiment_Model.pkl")
 
     # Evaluate
     accuracy = model.score(X_test, y_test)
