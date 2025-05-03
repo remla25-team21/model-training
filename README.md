852bbbd832e5e99c89390ebcf732d85ee212deb4
Yizhen Zang <Y.Zang-3@student.tudelft.nl>
2025-05-03 19:09:48 +0200

    Add training pipeline

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # model-training
+
+This repository contains the training pipeline for the sentiment analysis model used in our REMLA project. It uses the [lib-ml](https://github.com/remla25-team21/lib-ml) library for data preprocessing and saves the trained model as a release artifact. 
+
+
+## Dependencies
+
+Install the required dependencies:
+```bash
+pip install -r requirements.txt
+```
---
+++ train_model.py
@@ -0,0 +1,27 @@
+import pickle
+from libml.preprocessing import preprocess_train
+from sklearn.naive_bayes import GaussianNB
+
+
+def train_and_save_model(data_path, model_path, test_size=0.2, random_state=0):
+    # Preprocess using `lib-ml``
+    X_train, X_test, y_train, y_test = preprocess_train(data_path, test_size, random_state)
+
+    # Train model
+    model = GaussianNB()
+    model.fit(X_train, y_train)
+
+    # Evaluate
+    accuracy = model.score(X_test, y_test)
+    print(f"Model accuracy on test set: {accuracy:.2f}")
+
+    # Save model
+    with open(model_path, "wb") as f:
+        pickle.dump(model, f)
+    print(f"Trained model saved to {model_path}")
+
+if __name__ == "__main__":
+    train_and_save_model(
+        data_path="a1_RestaurantReviews_HistoricDump.tsv",
+        model_path="sentiment_model.pkl",
+    )
